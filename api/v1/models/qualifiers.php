7f028c01efeb3dccb02e0c5cce8e4d9616c6510f
Fady Abdelmohsen <fady.abdelmohsen@gmail.com>
2015-10-14 13:38:25 -0300

    changed db timestamps into strings :(

---
+++ api/v1/models/qualifiers.php
@@ -0,0 +1,40 @@
+<?php
+  class Qualifiers_model {
+
+    public function __construct(){
+      parent::__construct();
+    }
+
+    public function create_competition($db, $data)
+    {
+      $competition = $db->insert("Competitions", $data, array());
+      return $competition;
+    }
+
+    public function get_competitions($db) {
+      $data = $db->query("SELECT * FROM "."Competitions");
+      return $data;
+    }
+
+    public function get_competitions_for_season($db, $year) {
+      $data = $db->query("SELECT * FROM "."Competitions WHERE season_year = :year", array(':year' => $year));
+      
+      return $data;
+    }
+
+    public function update_competition($db, $data, $id)
+    {
+      $where_inCompetition = array('competition_id' => $id);    
+      $competition = $db->update("Competitions", $data, $where_inCompetition, array());
+      
+      return $competition;
+    }
+
+    public function delete_competition($db, $id) {
+      $competition = $db->delete("Competitions", 
+        array('competition_id' => $id));
+      
+      return  $competition;
+    }
+  }
+?>
