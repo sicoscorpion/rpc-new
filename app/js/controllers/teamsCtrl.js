f57e2da91874f0dd31a2711e9979a1976ee9a599
NSValleyMan <103801w@acadiau.ca>
2015-10-25 17:20:43 -0300

    Added some team stuffs

--- app/js/controllers/teamsCtrl.js
+++ app/js/controllers/teamsCtrl.js
@@ -1,9 +1,279 @@
 var app = angular.module('myApp');
 
 
-app.controller('teams_controller', ['$scope', '$location', function($scope, $location) {
+app.controller('teams_controller', ['$scope', '$location', 'Data', 'NgTableParams', '$modal', '$cookies', '$route', '$timeout', function($scope, $location, Data, NgTableParams, $modal, $cookies, $route, $timeout) {
 
     $scope.check_logged_in();
 
+    $scope.team = {
+        team_id: '',
+        local_id: '',
+        name: '',
+        organization: '',
+        approved: ''
+    }
+
+
+    Data.get("teams").then(function (result) {
+        if(result.status != 'error'){
+            console.log("Returned Teams for Admin: ", result);
+            $scope.seasons = result;
+            data = $scope.seasons;
+            $scope.teamTableParams = new NgTableParams({count: 10}, { data: data, counts: [1, 25, 50, 100]});
+        }
+    })
+
+    $scope.getTeams = function(){
+        Data.get("teams").then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Teams for Admin: ", result);
+                $scope.seasons = result;
+                data = $scope.seasons;
+                $scope.teamTableParams = new NgTableParams({count: 10}, { data: data, counts: [1, 25, 50, 100]});
+            }
+        })
+    }
+
+    $scope.getTeamsParticipate = function(team){
+        Data.get("teams/participate/" + team.team_id).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Teams participates: ", result);
+                $scope.participates = result;
+                data = $scope.participates;
+                $scope.teamPartTableParams = new NgTableParams({count: 10}, { data: data, counts: []});
+            }
+        })
+    }
+
+    $scope.getTeamsCoaches = function(team){
+        Data.get("manage/users/" + team.team_id).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Teams coaches: ", result);
+                $scope.coaches = result;
+                data = $scope.coaches;
+                $scope.teamCoachTableParams = new NgTableParams({count: 10}, { data: data, counts: []});
+            }
+        })
+    }
+
+    $scope.team_status = {
+        disabled: false,
+        message: 'Invaild form. Ensure team doesnt exist',
+        show_alert: false,
+        alert_type: 'warning round'
+    }
+
+    $scope.createTeam = function(team) {
+
+        $scope.team_status.message = 'Logging in';
+        $scope.team_status.disabled = true;
+        $scope.team_status.show_alert = true;
+        $scope.team_status.alert_type = 'info round';
+
+        
+        console.log("Creating Team: ", team);
+        console.log("Creating Team for user: ", $scope.getCookieData());
+
+        Data.post("teams", team).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Data from create team: ", result);
+                $scope.saved();
+                $scope.getTeams();
+
+
+            }else{
+                console.log("Error creating team", result);
+                $scope.team_status.message = 'Invaild form. Ensure team doesnt exist';
+                $scope.team_status.disabled = false;
+                $scope.team_status.alert_type = 'alert round';
+                $scope.team_status.show_alert = true;
+                $scope.fail();
+                
+            } 
+        }) 
+
+    }
+
+    $scope.setQual = function(qualifier){
+        console.log("Setting Qual: ", qualifier);
+
+        $scope.qualForTeam = qualifier;
+    }
+
+    $scope.setCoach = function(coach){
+        console.log("Setting coach: ", coach);
+
+        $scope.coachForTeam = coach;
+    }
+
+    $scope.addTeamQual = function(team, qual) {
+
+        console.log("Team: ", team);
+        console.log("Qual: ", qual);
+
+        $scope.part = {
+            team_id: team.team_id,
+            qual_id: qual.qual_id,
+            competition_id: qual.competition_id,
+            season_year: qual.season_year
+        }
+
+        $scope.team_status.message = 'Logging in';
+        $scope.team_status.disabled = true;
+        $scope.team_status.show_alert = true;
+        $scope.team_status.alert_type = 'info round';
+
+        
+        console.log("Adding Team qual: ", team);
+        console.log("Adding Team for user: ", $scope.getCookieData());
+
+        Data.post("teams/participate", $scope.part).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Data from add team part: ", result);
+                $scope.saved();
+                $route.reload();
+                $scope.modalInstance.dismiss('cancel');
+
+            }else{
+                $scope.modalInstance.dismiss('cancel');
+
+                console.log("Error creating team", result);
+                $scope.team_status.message = 'Invaild form. Ensure team doesnt exist';
+                $scope.team_status.disabled = false;
+                $scope.team_status.alert_type = 'alert round';
+                $scope.team_status.show_alert = true;
+                $scope.fail();
+                
+            } 
+        }) 
+
+    }
+
+    $scope.addTeamCoach = function(team, coach) {
+
+        console.log("Team: ", team);
+        console.log("coach: ", coach);
+
+        $scope.part = {
+            team_id: team.team_id,
+            user_id: coach.user_id
+        }
+
+        $scope.team_status.message = 'Logging in';
+        $scope.team_status.disabled = true;
+        $scope.team_status.show_alert = true;
+        $scope.team_status.alert_type = 'info round';
+
+        
+        console.log("Adding Team coach: ", team);
+        console.log("Adding Team for user: ", $scope.getCookieData());
+
+        Data.post("/manage", $scope.part).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Data from add team coach: ", result);
+                $scope.saved();
+                $route.reload();
+                $scope.modalInstance.dismiss('cancel');
+
+            }else{
+                $scope.modalInstance.dismiss('cancel');
+
+                console.log("Error creating team", result);
+                $scope.team_status.message = 'Invaild form. Ensure team doesnt exist';
+                $scope.team_status.disabled = false;
+                $scope.team_status.alert_type = 'alert round';
+                $scope.team_status.show_alert = true;
+                $scope.fail();
+                
+            } 
+        }) 
+
+    }
+
+    $scope.deleteTeamPart = function(team) {
+        
+        console.log("deleting part : ", team);
+        var path = "teams/participate/" + team.team_id + "/" + team.qual_id + "/" + team.competition_id + "/" + team.season_year;
+        Data.delete(path).then(function (result) {
+            if(result.status != 'error'){
+                console.log("Returned Data from delete team part: ", result);
+                $scope.saved();
+                $route.reload();
+                $scope.modalInstance.dismiss('cancel');
+
+
+            }else{
+                console.log("Error deleting team part: ", result);
+                $scope.fail();
+
+            } 
+        }) 
+        // $route.reload();  
+
+    }
+
+
+
+
+            // MODAL WINDOW
+    $scope.openTeams = function () {
+        console.log("Open Team Modal");
+
+
+        $scope.modalInstance = $modal.open({
+          controller: "teams_controller",
+          templateUrl: 'newTeam.html',
+          scope: $scope,
+          resolve: {
+                team: function()
+                {
+                    return $scope;
+                }
+            }
+
+        });
+    };
+
+
+            // MODAL WINDOW
+    $scope.updateTeamQual = function (team) {
+        console.log("Open TeamQual Modal");
+        $scope.t = team;
+        $scope.getTeamsParticipate(team);
+
+        $scope.modalInstance = $modal.open({
+          controller: "teams_controller",
+          templateUrl: 'teamQuals.html',
+          scope: $scope,
+          resolve: {
+                teamQual: function()
+                {
+                    return $scope;
+                }
+            }
+
+        });
+    };
+
+
+            // MODAL WINDOW
+    $scope.updateTeamCoach = function (team) {
+        console.log("Open Team Coach Modal");
+        $scope.t = team;
+        $scope.getTeamsCoaches(team);
+
+        $scope.modalInstance = $modal.open({
+          controller: "teams_controller",
+          templateUrl: 'teamCoach.html',
+          scope: $scope,
+          resolve: {
+                teamCoach: function()
+                {
+                    return $scope;
+                }
+            }
+
+        });
+    };
 
 }]);
